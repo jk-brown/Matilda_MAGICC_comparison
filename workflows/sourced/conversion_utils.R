8bde07fa8226bc5a43a929047ab0430402127e99
Joe Brown <joseph.brown.k@gmail.com>
2024-11-09 13:47:18 -0500

    Adding function to convert GCAM emissions to Hector emissions (GHG not LUC)
    First attempt at this function. Should convert all GHG emissions (global) to Hector inputs.

--- workflows/sourced/conversion_utils.R
+++ workflows/sourced/conversion_utils.R
@@ -2,4 +2,85 @@
 
 # 1 converting GCAM emissions to Hector emissions -------------------------
 
+get_hector_emissions <- function(gcam_emissions_data){
+
+  # pull out the emissions data from the dat_file
+  # gcam_df <- gcam_data$GCAM$`all emissions by region`
+  # 
+  # # aggregate regional emissions to get global emissions totals
+  # global_emissions <- aggregate(value ~ year + ghg,
+  #                               data = gcam_df,
+  #                               FUN = sum)
+  
+  # Global data 
+  global_emissions <- gcam_emisisons_data
+  
+  # get emissions mapping information
+  emissions_map <- read.csv("workflows/data/raw-data/GCAM_hector_emissions_map.csv") 
+  
+  # merge emissions_map with global_emissions
+  gcam_emissions_map <- merge(global_emissions, emissions_map, 
+                              by = "ghg", 
+                              all.x = TRUE) # all.x = T ensures NAs are added where ghg in global_emissions is not in emissions_map
+  setDT(gcam_emissions_map)
+  
+  # check that expected emissions are being passed to Hector.
+  # Expect NAs for:  H2, H2_AWB, PM10, PM2.5, and I think CO2_FUG
+  # TODO: what to do in CO2_FUG situation
+  missing_ghgs <- unique(gcam_emissions_map[is.na(gcam_emissions_map$agg.gas), ]$ghg)
+  expected_missing_ghgs <- c("H2", "H2_AWB", "PM10", "PM2.5")
+  
+  # check for the presence of expected_missing_ghgs in missing_ghgs.
+  # if any of the expected missing ghgs are not found in missing_ghgs, send an error. 
+  if(!all(expected_missing_ghgs %in% missing_ghgs)) {
+    stop ("Some emissions being passed to Hector are unexpected. These emissions may not be in Hector.")
+  }
+  
+  # convert gcam emissions to to hector emissions:
+  # 1 unit conversion
+  gcam_emissions_map$converted_value <- gcam_emissions_map$value * gcam_emissions_map$unit.conv
+  
+  # 2 Halocarbon ghgs can be aggregated into a single halocarbon category
+  sum_halocarbon <- gcam_emissions_map[, list(value = sum(converted_value)), 
+                                       by = c("agg.gas", "hector.name", "year", "hector.units")]
+  
+  # omit NAs from the halocarbon aggregate
+  gcam_emissions_input <- na.omit(sum_halocarbon)
+  
+  # Check for important columns
+  required_columns <- c("year", "value")
+  if (!all(required_columns %in% names(gcam_emissions_input))){
+    stop("hmmm, there is an important column missing is there a year and value column present?")
+  }
+  
+  # establish data years; 2005:2100. Before 2005 Hector uses GCAM inputs
+  data_years <- data.table(year = 2005:2100)
+  
+  # TODO: Is there a better way to do the following lines?
+  # Construct data frame of all the variables for the 2005:2100 year range.
+  # This code creates a data frame with NAs when no GCAM emissions are available.
+  # The NAs will be subsequently infilled.
+  columns_to_save <- names(gcam_emissions_input)[!names(gcam_emissions_input) %in% required_columns]
+  data_to_replicate <- distinct(gcam_emissions_input[, ..columns_to_save])
+  data_with_target_years <- repeat_add_columns(x = data_to_replicate, y = data_years)
+  NA_emissions <- gcam_emissions_input[data_with_target_years, on = names(data_with_target_years), nomatch = NA]
+  
+  # Replace emission NAs with approximated values 
+  approximated_emissions <- NA_emissions %>% 
+    group_by(hector.name, hector.units) %>% 
+    mutate(value = ifelse(is.na(value), 
+                          approx(year, value, xout = year, rule = 2)$y, 
+                          value)) %>% 
+    ungroup() %>% 
+    setDT()
+  
+  # construct final output
+  hector_emissions <- approximated_emissions[, .(scenario = "GCAM",
+                                                 variable = hector.name,
+                                                 year = year,
+                                                 value = value,
+                                                 units = hector.units)]
+  return(hector_emissions)
+}
+
 
