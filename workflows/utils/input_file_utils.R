ef255143dbb3cea73085ca6a5cb47d37b8a80bf0
Joe Brown <joseph.brown.k@gmail.com>
2024-11-11 15:43:09 -0500

    workflow 01_data_preprocessing_and_conversion
    Adding the first piece of the workflow.
    
    This take NDC experiment emissions data from GCAM and converts them to Hector formatted emissions data and save it in a new directory

--- workflows/utils/input_file_utils.R
+++ workflows/utils/input_file_utils.R
@@ -54,8 +54,8 @@ build_emissions_constraints_data <- function(hector_emissions_df,
   # define file path 
   file_path = file.path(directory, paste0(scenario_name, ".csv"))
   
-  # # write the updated data to a new csv file
-  # write.csv(emissions_constraint_update, file_path, row.names = F)
+  # write the updated data to a new csv file
+  write.csv(emissions_constraint_update, file_path, row.names = F)
 }
 
 
