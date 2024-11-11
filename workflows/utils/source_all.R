06444379c2aaafb9807f32024ac4c32a681d8054
Joe Brown <joseph.brown.k@gmail.com>
2024-11-11 14:24:57 -0500

    Adding source_all file

---
+++ workflows/utils/source_all.R
@@ -0,0 +1,6 @@
+## source set-up and all utils
+
+source("workflows/utils/conversion_utils.R")
+source("workflows/utils/input_file_utils.R")
+source("workflows/utils/preprocessing_utils.R")
+source("workflows/utils/set-up.R")
