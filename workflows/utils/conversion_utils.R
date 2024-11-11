089f2c9bf6074aeb071785ecb629667a85dd949c
Joe Brown <joseph.brown.k@gmail.com>
2024-11-11 13:12:20 -0500

    reorganizing file structure

--- workflows/utils/conversion_utils.R
+++ workflows/utils/conversion_utils.R
@@ -160,14 +160,3 @@ get_luc_emissions <- function(gcam_emissions_file) {
   return(annual_luc)
 }
 
-# EXTRA -------------------------------------------------------------------
-#consider adding as a function to check the emissions that are present in GCAM data that are not in Hector map.
-
-# Identify values in df1$ghg that are not in df2$ghg
-unique_values_not_in_df2 <- test_df$ghg[!test_df$ghg %in% hector_emiss$ghg]
-
-# Get unique values
-unique_values_not_in_df2 <- unique(unique_values_not_in_df2)
-
-# Print the result
-unique_values_not_in_df2
