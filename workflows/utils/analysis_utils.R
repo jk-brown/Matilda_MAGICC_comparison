41a3b46df7e130c20c17538735d7f51c92f33e2a
Joe Brown <joseph.brown.k@gmail.com>
2024-11-12 21:42:13 -0500

    Adding normalization function to analysis utils and weighting temperature metrics

--- workflows/utils/analysis_utils.R
+++ workflows/utils/analysis_utils.R
@@ -3,6 +3,23 @@
 # 1 Running Hector/Matilda in parallel ------------------------------------
 
 
-# 2 -----------------------------------------------------------------------
+# 2 Normalize data to reference period -----------------------------------------------------------------------
 
-
+normalize_dat <- function(data, ref_start, ref_end) {
+  
+  # Filter data for the reference period
+  ref_period <- subset(
+    data,
+    year >= ref_start &
+      year <= ref_end
+  )
+  
+  # Calculate the mean for the reference period
+  mean_ref_dat <- mean(ref_period$value, na.rm = TRUE)
+  
+  # Normalize values by subtracting the mean_ref_period
+  norm_dat <- data$value - mean_ref_dat
+  
+  # Return the normalized data
+  return(norm_dat)
+}
