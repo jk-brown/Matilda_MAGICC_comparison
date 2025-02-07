9d52fd86161973859f159d8dc3e9b9e0b605c72e
Joe Brown <joseph.brown.k@gmail.com>
2025-02-07 13:31:30 -0500

    Adding new plot that shows filered Hector ensemble.
    This code adds the figure with a description added to the README_figs document.

--- workflows/utils/magicc_cmip6_mapping.R
+++ workflows/utils/magicc_cmip6_mapping.R
@@ -2,16 +2,16 @@
 
 # Mapping for CMIP6 data 
 
-cmip6_dat <- read.csv("workflows/data/raw-data/cmip6_rslts.csv") %>% 
+cmip6_dat <- read.csv("data/raw-data/cmip6_rslts.csv") %>% 
   select(-X)
 
 # Mapping for Ou and Iyer et al. 2021 data (using MAGICC for median warming trajectory)
-ou_data <- read.csv("workflows/data/raw-data/ou_Iyer_global_mean_temperature.csv")
+ou_data <- read.csv("data/raw-data/ou_Iyer_global_mean_temperature.csv")
 
 # Mapping for estimated MAGICC median and uncertainty from online MAGICC resource
 
 ## Median trajectory for BAU scenario
-magicc_bau <- read.csv("workflows/data/raw-data/T_01_BAU_magicc.csv")
+magicc_bau <- read.csv("data/raw-data/T_01_BAU_magicc.csv")
 
 ## MAGICC uncertainty 
-magicc_unc <- read.csv("workflows/data/raw-data/magicc_bau_unc.csv")
+magicc_unc <- read.csv("data/raw-data/magicc_bau_unc.csv")
